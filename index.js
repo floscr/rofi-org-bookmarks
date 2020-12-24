497d54480d32e780e19feb7d54de68a5fefa3fdd
Florian Schroedl <flo.schroedl@gmail.com>
2020-12-24 16:24:37 +0100

    Transform ast

--- index.js
+++ index.js
@@ -2,15 +2,41 @@ const R = require('ramda');
 const S = require('sanctuary');
 const Orga = require('orga');
 const fs = require('fs');
+const $ = require('sanctuary-def');
+
+const pipeWhileNotNil = R.pipeWith(
+  (f, res) => (R.isNil(res) ? res : f(res)),
+);
+
+const prepareItem = (item) => {
+  const headline = item.children[0];
+  const tags = headline.children.find((x) => x.type === 'tags');
+
+  const { properties } = item;
+
+  return {
+    headline: headline.content,
+    tags: tags ? tags.tags : [],
+    properties,
+  };
+};
 
 const parse = S.pipe([
   (x) => x.replace('~', process.env.HOME),
   (x) => fs.readFileSync(x, 'utf8'),
+
   Orga.parse,
+
   ({ children }) => children,
   S.head,
   S.map(S.prop('children')),
-  R.tap((x) => console.log(x.value[1].properties)),
+  S.chain(S.tail),
+
+  S.map((xs) => xs.map(prepareItem)),
+  // S.map((xs) => xs.map((x) => x.children[0].children)),
+
+  R.tap((x) => console.log(x.value)),
+
 ]);
 
 parse('~/Documents/Org/Bookmarks/bookmarks.org');
